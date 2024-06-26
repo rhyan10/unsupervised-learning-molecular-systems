bf8607769751d1660205a9fcd077f36266a0a203
Toni Oestereich <83869393+ToOest@users.noreply.github.com>
2024-06-26 20:14:23 +0200

    Add files via upload
    Tested and fixed environment install, clean up of scripts

--- 2-Dimensionality-Reduction/setup.sh
+++ 2-Dimensionality-Reduction/setup.sh
@@ -6,9 +6,11 @@ wget http://quantum-machine.org/data/qm7.mat
 
 # Download auxiliary scripts to the current env
 shred -u molecule_visualizer.py
-# wget https://raw.githubusercontent.com/rhyan10/CECAM_unsupervised_learning_best_practices/main/utils/view_molecule.py -O view_molecule.py
+wget https://github.com/rhyan10/CECAM_unsupervised_learning_best_practices/blob/main/utils/view_molecule.py -O view_molecule.py
 
 # Install and update required packages
 pip install pacmap -U
 pip install py3Dmol -U
-pip install sklearn -U
+pip install scikit-learn -U
+pip install pandas -U
+pip install seaborn -U
